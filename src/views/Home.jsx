259d49f049cc2e7cf1b14ef8bb4f6575c76049c7
QuyYeuCode <dangquyngoclap@gmail.com>
2024-03-02 15:05:48 +0700

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import AddButton from "../components/AddButton";
+import CreateProject from "../components/CreateProject";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import { loadProjects } from "../services/blockchain";
+import { useGlobalState } from "../store";
+
+const Home = () => {
+  const [projects] = useGlobalState("projects");
+
+  useEffect(() => {
+    const loadPrj = async () => {
+      await loadProjects();
+    };
+    loadPrj();
+  }, []);
+  return (
+    <>
+      <Hero />
+      <Projects projects={projects} />
+      <CreateProject />
+      <AddButton />
+    </>
+  );
+};
+
+export default Home;
